acc8f39cc9bcd52b577f6b2cd07696f0245e6719
GBDM97 <ghian.m@gmail.com>
2022-10-31 20:52:00 -0300

    removed comments

--- get.java
+++ get.java
@@ -10,9 +10,6 @@ class get {
         
             HttpRequest request = HttpRequest.newBuilder()
             .uri(URI.create("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=A8S6DY62W9QUB7TW"))
-            // .header("X-RapidAPI-Proxy-Secret", "a755b180-f5a9-11e9-9f69-7bf51e845926")
-            // .header("X-RapidAPI-Key", "3ea29cdb0dmsheb3c3e5bb38b7acp1523f0jsn8ce44ede4069")
-            // .header("X-RapidAPI-Host", "stock-and-options-trading-data-provider.p.rapidapi.com")
             .method("GET", HttpRequest.BodyPublishers.noBody())
             .build();
     HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
