4b4e0894dd6a4c8a398066086a8a56cead59f2e5
GBDM97 <ghian.m@gmail.com>
2022-10-31 22:15:03 -0300

    scanner added

--- get.java
+++ get.java
@@ -3,13 +3,21 @@ import java.net.http.HttpClient;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 
 class get {
     public static void main(String[] args)throws IOException, InterruptedException{
-        
+        System.out.println("Enter Stock Ticker:");
+        Scanner ticker = new Scanner(System.in);
+        String u1 = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=";
+        String u2 = ticker.next();
+        String u3 = "&interval=5min&apikey=A8S6DY62W9QUB7TW";
+        String u = u1+u2+u3;
+        ticker.close();
+
             HttpRequest request = HttpRequest.newBuilder()
-            .uri(URI.create("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=A8S6DY62W9QUB7TW"))
+            .uri(URI.create(u))
             .method("GET", HttpRequest.BodyPublishers.noBody())
             .build();
     HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
